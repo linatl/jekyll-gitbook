9d06c635ed6f512e11da5aaf5ef41f12641dbb4b
sKyW1per <47339295+sKyW1per@users.noreply.github.com>
2024-05-10 22:45:50 +0200

    Update h7689h780h708h0.js

--- _includes/h7689h780h708h0.js
+++ _includes/h7689h780h708h0.js
@@ -1,14 +1,1 @@
-function runthething() {
-var msdelay = 1000;
-    function clickButton(msdelay) {
-        setTimeout(function() {
-            var button = document.getElementById("troop_confirm_submit");
-            if (button) {
-                button.click();
-            } else {
-                alert("Button not found with name: " + buttonName);
-            }
-        }, msdelay)
-    };
-clickButton("troop_confirm_submit", msdelay);
-}();
+
